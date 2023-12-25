c41a33113d5bfb8096d091725109533077dd8d76
Roni003 <ronikazmirci003@gmail.com>
2023-12-26 01:22:07 +0300

    fixed config

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = { 
+    Prefix: "?",
+    Users: { 
+        OWNERS: ["000000000000"], // Bot owner's discord ID (fill with your id)
+        OWNER_NAME: "John" // Bot owner's name (fill with your name)
+    }, 
+    Handlers: { 
+        MONGO: "" 
+    }, 
+    Client: { 
+        TOKEN: "",  // Bot's token
+        ID: "000000000000", // Bot's discord ID
+        GUILD_ID: "000000000000" // Bot's discord server's ID
+    } 
+}
